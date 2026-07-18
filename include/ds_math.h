6b4ea3e5fbe7cebb9b9832dd2f0c83e8f1b75358
Axel Sandstedt <axel.sandstedt97@protonmail.com>
2026-07-18 02:52:17 +0200

    Added Relative/ToLocal operations for ds_Transform

--- include/ds_math.h
+++ include/ds_math.h
@@ -49,6 +49,37 @@ static inline ds_Transform ds_TransformIdentity(void)
     return transform;
 }
 
+/* Return the relative transform of target to reference:
+ *
+ *      ref->rot*rel->rot = ref->rot*ref->rot_inv*T->rot = target->rot
+ *
+ *      ref->rot*rel->pos + ref->pos = ref->rot*ref->rot_inv*(T->pos - ref->pos) + ref->pos = target->pos
+ */
+static inline ds_Transform ds_TransformRelative(const ds_Transform *reference, const ds_Transform *target)
+{
+    ds_Transform relative;
+    quat inv;
+
+    QuatInverse(inv, reference->rotation);
+    QuatMul(relative.rotation, inv, target->rotation);
+    Vec3Sub(relative.position, target->position, reference->position);
+    QuatVec3RotateSelf(relative.position, inv);
+
+    return relative;
+}
+
+/* Return the local position of target within reference frame:
+ *
+ *      ref->rot*rel + ref->pos = ref->rot*ref->rot_inv*(target - ref->pos) + ref->pos = target
+ */
+static inline void ds_TransformPointToLocal(vec3 local, const ds_Transform *reference, const vec3 target)
+{
+    quat inv;
+    QuatInverse(inv, reference->rotation);
+    Vec3Sub(local, target, reference->position);
+    QuatVec3RotateSelf(local, inv);
+}
+
 #ifdef __cplusplus
 } 
 #endif
